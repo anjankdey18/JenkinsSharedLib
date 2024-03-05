ec3ba85250a2395400eebbc54bc1cae4729e3994
anjankdey18 <anjan.k.dey18@gmail.com>
2024-03-05 18:17:15 -0500

    updating jenkins file

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,6 +1,7 @@
 def call(credentialsId){
 
-    withSonarQubeEnv(credentialsId: credentialsId) {
+    // withSonarQubeEnv(credentialsId: credentialsId) {
+    withSonarQubeEnv(credentialsId: 'sonar-api') {
          sh 'mvn clean package sonar:sonar'
     }
 }
